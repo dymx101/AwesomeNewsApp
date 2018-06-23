7c6ef91a19b3a7a29f5577bc64bcf31119adf271
Yiming Dong <yimingdong@Yimings-MacBook-Pro.local>
2018-06-23 16:59:09 +0800

    Add ViewControllers

---
+++ AwesomeNewsApp/AwesomeNewsApp/NewsDetail/Controllers/NewsDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsDetailViewController.swift
+//  AwesomeNewsApp
+//
+//  Created by Yiming Dong on 2018/6/23.
+//  Copyright © 2018 Yiming Dong. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ AwesomeNewsApp/AwesomeNewsApp/NewsList/Controllers/NewsListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AwesomeNewsApp
+//
+//  Created by Yiming Dong on 2018/6/23.
+//  Copyright © 2018 Yiming Dong. All rights reserved.
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
