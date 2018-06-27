8226fa3f3187ea4c6f47d3bb0ba5e94f231e0698
Yiming Dong <yimingdong@Yimings-MacBook-Pro.local>
2018-06-27 20:12:16 +0800

    Implementing UI Tests

--- AwesomeNewsApp/AwesomeNewsAppUITests/Cases/AwesomeNewsAppUITests.swift
+++ AwesomeNewsApp/AwesomeNewsAppUITests/Cases/AwesomeNewsAppUITests.swift
@@ -9,28 +9,62 @@
 import XCTest
 
 class AwesomeNewsAppUITests: XCTestCase {
-        
+    
+    let app = XCUIApplication()
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCanGoToNewsDetailFromNewsList() {
+        
+        app.tables.cells.element(boundBy: 0).tap()
+        
+        let newsDetailLabel = app.otherElements["newsDetailView"]
+//        print(app.debugDescription)
+        XCTAssertTrue(newsDetailLabel.exists, "Should go to news detail page")
     }
     
+    func testCanSearchAndGetCorrectResults() {
+
+        app.navigationBars["Awesome News"].buttons["Search"].tap()
+        
+        let searchForNewsSearchField = app.searchFields["Search for news"]
+        searchForNewsSearchField.tap()
+        
+        let aKey = app/*@START_MENU_TOKEN@*/.keys["A"]/*[[".keyboards.keys[\"A\"]",".keys[\"A\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        aKey.tap()
+        
+        let bKey = app/*@START_MENU_TOKEN@*/.keys["b"]/*[[".keyboards.keys[\"b\"]",".keys[\"b\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        bKey.tap()
+        
+        let cKey = app/*@START_MENU_TOKEN@*/.keys["c"]/*[[".keyboards.keys[\"c\"]",".keys[\"c\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        cKey.tap()
+
+        app/*@START_MENU_TOKEN@*/.buttons["Go"]/*[[".keyboards.buttons[\"Go\"]",".buttons[\"Go\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let newsItemTitle = app.tables.cells.element(boundBy: 0).staticTexts["newsItemTitle"]
+        waitForElementToAppear(element:newsItemTitle)
+        
+        XCTAssertTrue(newsItemTitle.label.contains("Abc"))
+    }
+    
+    func waitForElementToAppear(element: XCUIElement, timeout: TimeInterval = 5,  file: String = #file, line: UInt = #line) {
+        let existsPredicate = NSPredicate(format: "exists == true")
+        
+        expectation(for: existsPredicate,
+                    evaluatedWith: element, handler: nil)
+        
+        waitForExpectations(timeout: timeout) { (error) -> Void in
+            if (error != nil) {
+                let message = "Failed to find \(element) after \(timeout) seconds."
+                self.recordFailure(withDescription: message, inFile: file, atLine: Int(line), expected: true)
+            }
+        }
+    }
 }
